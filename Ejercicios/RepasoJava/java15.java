de51228bfdea3ad6287a62accc635b12c23fc332
Victor Ramirez <victor67ugr@gmail.com>
2026-09-23 09:37:00 +0200

    ej15

---
+++ Ejercicios/RepasoJava/java15.java
@@ -0,0 +1,9 @@
+public class java15 {
+    public static void main(String[] args) {
+        System.out.println("EJERCICIO 15: METODO DE SALUDO.");
+        String nombre = "Victor";
+        String saludo = "Hola";
+        System.out.println(saludo + ", " + nombre + "!");
+    }
+    
+}

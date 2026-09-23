bd3a6f662de5f2302e610f7d0b4b411a95ac5a1e
Victor Ramirez <victor67ugr@gmail.com>
2026-09-23 08:45:14 +0200

    ej4

---
+++ Ejercicios/RepasoJava/java4.java
@@ -0,0 +1,14 @@
+public class java4 {
+    public static void main(String[] args) {
+        System.out.println("Ejercicio 4: Operadores.");
+        int numero1=5;
+        int numero2=3;
+
+        System.out.println("Suma: " + (numero1 + numero2));
+        System.out.println("Resta: " + (numero1 - numero2));
+        System.out.println("Multiplicacion: " + (numero1 * numero2));
+        System.out.println("Division: " + (numero1 / numero2));
+        System.out.println("Módulo: " + (numero1 % numero2));
+    }
+    
+}

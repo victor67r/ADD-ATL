4b7d0656ea6609630a0bc0b337d85b58e9bdf180
Victor Ramirez <victor67ugr@gmail.com>
2026-09-23 08:34:59 +0200

    ej2

--- Ejercicios/RepasoJava/java2.java
+++ Ejercicios/RepasoJava/java2.java
@@ -4,5 +4,19 @@ public class java2 {
 
     public static void main(String[] args) {
         System.out.println("Ejercicio 2: Conversion de tipos.");
+        String numeroTexto = "123";
+        double precio = 19.99;
+
+        //Convertir numeroTexto a entero
+        int numeroConvertido = Integer.parseInt(numeroTexto);
+        System.out.println("Numero convertido a entero: " + numeroConvertido);
+
+        //Convertir precio a entero
+        int numero= (int) precio;
+        System.out.println("Precio convertido a entero: " + numero);
+
+        //Convierte numero a String
+        String numeroString = Integer.toString(numeroConvertido);
+        System.out.println("Numero convertido a String: " + numeroString);
     }
 }

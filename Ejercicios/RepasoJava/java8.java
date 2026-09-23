a0ab0ff62904418e3cca9c7523eb1f7f23b59044
Victor Ramirez <victor67ugr@gmail.com>
2026-09-23 09:08:39 +0200

    ej8

---
+++ Ejercicios/RepasoJava/java8.java
@@ -0,0 +1,34 @@
+public class java8 {
+    public static void main(String[] args) {
+        System.out.println("Ejercicio 8: DIA DE LA SEMANA");
+        int diaSemana=6;
+        switch (diaSemana){
+            case 1:
+                System.out.println("Lunes");
+                break;
+            case 2:
+                System.out.println("Martes");
+                break;
+            case 3:
+                System.out.println("Miercoles");
+                break;
+            case 4:
+                System.out.println("Jueves");
+                break;
+            case 5:
+                System.out.println("Viernes");
+                break;
+            case 6:
+                System.out.println("Sabado");
+                break;
+            case 7:
+                System.out.println("Domingo");
+                break;
+            default:
+                System.out.println("Dia de la semana no valido.");
+        }
+        
+        
+    }
+    
+}

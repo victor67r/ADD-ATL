61ffa9ea92754498606ac05433fcc55a342ee58d
Victor Ramirez <victor67ugr@gmail.com>
2026-09-23 08:16:07 +0200

    ej1

---
+++ Ejercicios/Repaso Java/java1.java
@@ -0,0 +1,13 @@
+package Repaso.Java;
+
+public class java1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Ejercicio 1: Variables.");
+
+        int edad = 29;
+
+    }
+
+}

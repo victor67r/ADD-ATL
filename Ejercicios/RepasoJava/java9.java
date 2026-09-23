0f789897e86861ed83428e2436631e55e688edc8
Victor Ramirez <victor67ugr@gmail.com>
2026-09-23 09:10:09 +0200

    ej9

---
+++ Ejercicios/RepasoJava/java9.java
@@ -0,0 +1,10 @@
+public class java9 {
+    public static void main(String[] args) {
+        System.out.println("Ejercicio 9: TABLA DE MULTIPLICAR.");
+        int numero = 5;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(numero + " x " + i + " = " + (numero * i));
+        }
+    }
+    
+}

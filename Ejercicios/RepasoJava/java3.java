6abb1193e69c3b76bdb60081a7c7ccc173ffeef0
Victor Ramirez <victor67ugr@gmail.com>
2026-09-23 08:38:52 +0200

    ej3

---
+++ Ejercicios/RepasoJava/java3.java
@@ -0,0 +1,12 @@
+public class java3 {
+    public static void main(String[] args) {
+        System.out.println("Ejercicio 3: Constantes.");
+
+
+        final double PI = 3.14159;
+        final double C = 299792458;
+        final String NOMBRE_APLICACION="Mi App Java";
+        
+    }
+    
+}

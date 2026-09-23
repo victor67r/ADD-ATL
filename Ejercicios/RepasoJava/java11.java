4c7f445536a1ba6b1ae245bf8e7f46de64eaf08d
Victor Ramirez <victor67ugr@gmail.com>
2026-09-23 09:21:34 +0200

    ej11

---
+++ Ejercicios/RepasoJava/java11.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class java11 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Ejercicio 11: DECLARACION E INICIALIZACION DE VARIABLES.");
+        int enteros[] = {1, 2, 3, 4, 5};
+        System.out.println("Array enteros: " + Arrays.toString(enteros));
+
+        String diaSemana[] = {"Lunes", "Martes", "Miércoles", "Jueves", "Viernes"};
+        System.out.println("Array diaSemana: " + Arrays.toString(diaSemana));
+
+double temperaturas[] = {20.5, 22.3, 19.8, 21.0, 23.1, 24.7, 18.9, 25.3, 27.1, 22.8};
+        System.out.println("Array temperaturas: " + Arrays.toString(temperaturas));
+    }
+}
